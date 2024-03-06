d01653b57f13d0ca253f03c34bf1232b0ddac726
Manahil Aamir <112062135+Manahil-Aamir@users.noreply.github.com>
2024-03-06 09:56:11 +0500

    changes

---
+++ lib/product.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+//import 'package:productdisplay/models/product_model.dart';
+
+class ProductDisplay extends StatefulWidget {
+  const ProductDisplay({super.key});
+
+
+  @override
+  State<ProductDisplay> createState() => _ProductDisplayState();
+}
+
+class _ProductDisplayState extends State<ProductDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
